1f04b63e91ae07a5351bf63809b702e3c582fffe
stepann <stepan.snegovskoi@mail.ru>
2025-02-09 21:50:34 +0300

    Add Room-core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.converter)
 
+    // Room
+    implementation(libs.room.core)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
