eb6e446341452f4e95ac871b9d0650c15c1ccfb1
stepann <stepan.snegovskoi@mail.ru>
2025-02-09 21:33:47 +0300

    Add MVIKotlin-core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 
 dependencies {
 
+    //MVIKotlin
+    implementation(libs.mvikotlin.core)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
