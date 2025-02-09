db158be1ed7eee4906810eac9d5258073d71b1bd
stepann <stepan.snegovskoi@mail.ru>
2025-02-09 21:56:18 +0300

    Add Icons-extended

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,9 @@ dependencies {
     // Glide
     implementation(libs.glide)
 
+    // Icons
+    implementation(libs.icons)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
