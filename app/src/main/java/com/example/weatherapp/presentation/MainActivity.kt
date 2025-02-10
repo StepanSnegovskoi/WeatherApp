b2997b68bea0425a24aebf2c5c25bb19009c7926
stepann <stepan.snegovskoi@mail.ru>
2025-02-10 21:31:58 +0300

    Add RootContent in MainActivity

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -3,14 +3,31 @@ package com.example.weatherapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.defaultComponentContext
+import com.example.weatherapp.WeatherApp
+import com.example.weatherapp.presentation.root.DefaultRootComponent
+import com.example.weatherapp.presentation.root.RootContent
 import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var rootComponentFactory: DefaultRootComponent.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as WeatherApp).applicationComponent.inject(this)
+
         super.onCreate(savedInstanceState)
+
+        val rootComponent = rootComponentFactory.create(
+            componentContext = defaultComponentContext()
+        )
+
         setContent {
             WeatherAppTheme {
-                
+                RootContent(component = rootComponent)
             }
         }
     }
