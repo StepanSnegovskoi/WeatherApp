cac2d425ee4164e64267719c6903b189b67dd8d2
stepann <stepan.snegovskoi@mail.ru>
2025-02-10 14:09:20 +0300

    Create Search mapper

---
+++ app/src/main/java/com/example/weatherapp/data/mapper/SearchMapper.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.data.mapper
+
+import com.example.weatherapp.data.dto.CityDto
+import com.example.weatherapp.domain.entity.City
+
+fun CityDto.toEntity() = City(id, name, country)
+
+fun List<CityDto>.toEntities() = map { it.toEntity() }
